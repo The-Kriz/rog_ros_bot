a323350e0a9c1311148689eafa59bd893e4d9ef1
The-Kriz <harikrishnanm220b@gmail.com>
2023-09-09 23:39:52 +0530

    fixed RPLidar Error

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -9,12 +9,16 @@ def generate_launch_description():
         Node(
             package='rplidar_ros',
             executable='rplidar_composition',
+            name='rplidar_node',
             output='screen',
             parameters=[{
-                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0',
-                'frame_id': 'laser_frame',
-                'angle_compensate': True,
-                'scan_mode': 'Standard'
-            }]
+                        'channel_type':'serial',
+                        'serial_port': '/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0',
+                        'serial_baudrate': 115200,
+                        'frame_id': 'laser_frame',
+                        'angle_compensate': True,
+                        'scan_mode': 'Standard'
+                        }]
         )
     ])
+
